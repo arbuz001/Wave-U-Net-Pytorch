f69b8b6e9fe6d74e4f8b4e2030956892a2224b31
Alexey Arbuzov <alexey.arbuzov@gmail.com>
2023-04-05 16:19:25 +0300

    - fixed script to start jupyter lab renaming

---
+++ start_remote_jupyter_lab.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+# https://towardsdatascience.com/how-to-connect-to-jupyterlab-remotely-9180b57c45bb
+# https://medium.com/mlearning-ai/set-up-remote-jupyter-lab-notebook-server-for-remote-browser-access-2cef464f203e
+# steps to launch jupyter lab on remote server
+# pip install jupyterlab
+# jupyter lab --generate-config
+# jupyter lab password
+# pwd = mlworkst@tion
+# to create config
+# nano nano /home/aarbuzov/.jupyter/jupyter_server_config.json
+# {  
+#   "NotebookApp": {
+#    "allow_remote_access":true,
+#    "ip":"192.168.112.60",
+#    "port":8888,
+#    "allow_root":true
+#   }
+# }
+
+
+jupyter lab --no-browser --notebook-dir /home/aarbuzov/work-and-study/git/Wave-U-Net-Pytorch/ 
